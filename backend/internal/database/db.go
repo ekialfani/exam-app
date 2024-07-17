5011c3a76ef09a772082121301ed6dafbe65960f
ekialfani <ekialfani15@gmail.com>
2024-07-17 20:17:27 +0800

    feat: setup backend with initial dependencies, server, and database configuration

---
+++ backend/internal/database/db.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"backend/internal/models"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var (
+	db *gorm.DB
+	err error
+)
+
+func StartDB() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("error loading .env file")
+	}
+
+	username := os.Getenv("DBUSER")
+	password := os.Getenv("DBPASSWORD")
+	host := os.Getenv("DBHOST")
+	port := os.Getenv("DBPORT")
+	dbName := os.Getenv("DBNAME")
+
+	config := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, dbName)
+
+	db, err = gorm.Open(mysql.Open(config), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println("berhasil terhubung ke database...")
+
+	db.Debug().AutoMigrate(models.Lecture{})
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
---
+++ backend/internal/routers/router.go
@@ -0,0 +1,9 @@
+package routers
+
+import "github.com/gin-gonic/gin"
+
+func StartServer() *gin.Engine {
+	router := gin.Default()
+
+	return router
+}
