fdc4c93d476330a16ffacb893f6110eee3de945d
ekialfani <ekialfani15@gmail.com>
2024-09-17 22:48:11 +0800

    feat(frontend): add ScrollView to ExamList page

--- frontend/src/pages/User/ExamList/index.js
+++ frontend/src/pages/User/ExamList/index.js
@@ -1,7 +1,14 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { Entypo } from "@expo/vector-icons";
 import { useEffect } from "react";
-import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllExamAssignments } from "../../../redux/slice/examSlice";
 import {
@@ -25,52 +32,55 @@ export const ExamList = () => {
   }, [isFocused, token, dispatch]);
 
   return (
-    <View className="items-center mt-5">
-      <View className="w-5/6">
-        <FlatList
-          data={exam?.examAssignments}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              className="w-full bg-white mb-3 flex-row items-center p-3 rounded-md shadow-md"
-              onPress={() =>
-                navigation.navigate("ExamDetail", { examId: item.exam_id })
-              }
-            >
-              <Image
-                className="w-16 h-16"
-                source={{
-                  uri: "https://th.bing.com/th/id/OIP.n4D17OyV7mU1IhjT6tnFgQHaE8?rs=1&pid=ImgDetMain",
-                }}
-              />
-              <View className="ml-3">
-                <Text className="capitalize font-medium">
-                  {item?.exam?.title}
-                </Text>
-                <Text className="text-xs font-medium mb-1">
-                  {item?.exam?.lecturer?.full_name}
-                </Text>
-                <Text className="text-xs text-slate-500">
-                  {item?.exam?.Questions?.length} pertanyaan
-                </Text>
-                <View className="flex-row items-center">
-                  <Text className="text-xs text-slate-500">
-                    {ParseDateToIndonesianFormat(
-                      new Date(item?.exam?.start_time)
-                    )}
+    <ScrollView>
+      <View className="items-center mt-5">
+        <View className="w-5/6">
+          <FlatList
+            scrollEnabled={false}
+            data={exam?.examAssignments}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                className="w-full bg-white mb-3 flex-row items-center p-3 rounded-md shadow-md"
+                onPress={() =>
+                  navigation.navigate("ExamDetail", { examId: item.exam_id })
+                }
+              >
+                <Image
+                  className="w-16 h-16"
+                  source={{
+                    uri: "https://th.bing.com/th/id/OIP.n4D17OyV7mU1IhjT6tnFgQHaE8?rs=1&pid=ImgDetMain",
+                  }}
+                />
+                <View className="ml-3">
+                  <Text className="capitalize font-medium">
+                    {item?.exam?.title}
                   </Text>
-                  <Entypo name="dot-single" color="#64748b" size={20} />
-                  <Text className="text-xs text-slate-500">
-                    {ParseTimeToIndonesianFormat(
-                      new Date(item?.exam?.start_time)
-                    )}
+                  <Text className="text-xs font-medium mb-1">
+                    {item?.exam?.lecturer?.full_name}
                   </Text>
+                  <Text className="text-xs text-slate-500">
+                    {item?.exam?.Questions?.length} pertanyaan
+                  </Text>
+                  <View className="flex-row items-center">
+                    <Text className="text-xs text-slate-500">
+                      {ParseDateToIndonesianFormat(
+                        new Date(item?.exam?.start_time)
+                      )}
+                    </Text>
+                    <Entypo name="dot-single" color="#64748b" size={20} />
+                    <Text className="text-xs text-slate-500">
+                      {ParseTimeToIndonesianFormat(
+                        new Date(item?.exam?.start_time)
+                      )}
+                    </Text>
+                  </View>
                 </View>
-              </View>
-            </TouchableOpacity>
-          )}
-        />
+              </TouchableOpacity>
+            )}
+          />
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
